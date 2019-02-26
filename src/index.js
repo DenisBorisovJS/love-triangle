e8385707494b9c7892ee9c233e5f0c6e7a06d0bb
Denis <denis360_93@mail.ru>
2019-02-26 23:50:38 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+* @param preferences - an array of integers. Indices of people, whom they love
+* @returns number of love triangles
+*/
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var trianglesValues = {};
+  var trianglesCount = 0;
+
+  preferences.forEach(function(value,key) {
+    trianglesValues[key+1] = value;
+  });
+
+  var matches = [];
+
+  for (var key in trianglesValues) {
+    if (matches.includes(trianglesValues[key])) {
+      continue;
+    }
+
+    var second = trianglesValues[key];
+    var third = trianglesValues[second];
+    var first = trianglesValues[third];
+
+    if (first == key && second != first && third != first) {
+      matches.push(first,second,third);
+      trianglesCount++;
+    }
+  }
+
+  return trianglesCount;
 };
